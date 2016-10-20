955ced3cd788692dc3c481fd130e141ebf2ad9e0
Eleazar <eleazardzdo@gmail.com>
2016-10-20 07:21:12 +0100

    initial structure

---
+++ src/main/java/turing_machine/model/TuringMachine.java
@@ -0,0 +1,41 @@
+/**
+ * CC Asignements - TuringMachine.java 19/10/16
+ * <p>
+ * Copyright 20XX Eleazar Díaz Delgado. All rights reserved.
+ */
+
+package main.java.turing_machine.model;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+/**
+ * TODO: Commenta algo
+ *
+ */
+public class TuringMachine {
+    /**
+     * Each header(case of turing multi header)
+     */
+    private ArrayList<Tape> tapes;
+    private Set<String>     states;
+    private Set<Character>  alphabet;
+    private char            blankSymbol;
+    private Transition      transitionTable;
+    private String          startState;
+    private Set<String>     endStates;
+
+
+    /**
+     * Build a Turing machine
+     */
+    public TuringMachine() {
+        // TODO: Check constraints to be a correct Turing machine
+        // endStates ⊂ states
+        // startState ∈ states
+        // blankSymbol ∉ alphabet
+        // Check transition table correctness
+    }
+
+
+}
